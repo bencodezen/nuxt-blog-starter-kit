b2d20d23d551eb61d2fb562e9554f41c6a1568cd
Ben Hong <ben@bencodezen.io>
2021-08-03 12:31:40 -0400

    feature: add simple blog post list

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<script>
+export default {}
+</script>
+
+<template>
+  <h1>About Page</h1>
+</template>
+
+<style></style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  async asyncData({ $content }) {
+    const postList = await $content('blog').fetch()
+    const filteredResults = postList.map((item) => ({
+      slug: item.slug,
+    }))
+
+    return {
+      filteredResults,
+    }
+  },
+}
+</script>
+
+<template>
+  <div>
+    <h1>Blog Page</h1>
+    <p>{{ filteredResults }}</p>
+  </div>
+</template>
+
+<style></style>
